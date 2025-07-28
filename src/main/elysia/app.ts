08a147ff883101eb7334b2a0901dc3a22675409b
jeseias <domingosjeseias@gmail.com>
2025-07-28 13:32:20 +0100

    feat: add domain

--- src/main/elysia/app.ts
+++ src/main/elysia/app.ts
@@ -11,7 +11,7 @@ import { protectedDocs } from "./swagger/swagger-config";
 
 export const app = new Elysia()
   .use(cors({
-    origin: ['https://djezyas.com'],
+    origin: ['https://djezyas.com', 'https://www.djezyas.com'],
     credentials: true,
     allowedHeaders: '*',
     methods: '*',
