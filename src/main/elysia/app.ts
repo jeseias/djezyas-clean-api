5480fd335eaa9afda999d3584c13a1e64df3fc0d
jeseias <domingosjeseias@gmail.com>
2025-07-29 13:11:00 +0100

    feat: add local host

--- src/main/elysia/app.ts
+++ src/main/elysia/app.ts
@@ -11,7 +11,7 @@ import { protectedDocs } from "./swagger/swagger-config";
 
 export const app = new Elysia()
   .use(cors({
-    origin: ['https://djezyas.com', 'https://www.djezyas.com', 'https://www.djezyas.com/'],
+    origin: ['https://djezyas.com', 'https://www.djezyas.com', 'https://www.djezyas.com/', 'http://localhost:3000'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token', 'Accept', 'Origin', 'X-Requested-With'], 
     methods: ['GET', 'POST', 'OPTIONS'],
